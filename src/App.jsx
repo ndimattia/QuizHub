6c3c462562a669b5a0144381e6e26eb8365e00e8
Noè Di Mattia <noedimattia@protonmail.com>
2024-11-13 16:20:19 +0100

    Addet Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './Button'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const buttonLabels = ["Alpha", "Bravo", "Charlie"]
+
+  const [smiley, setSmiley] = useState("thinking")
+
+  const checkAnswer = (event) => {
+    const clickedLabel = event.target.innerHTML
+
+    if(clickedLabel === "Alpha") {
+      setSmiley("happy")
+    } else {
+      setSmiley("sad")
+    }
+
+    console.log(clickedLabel)
+  }
 
   return (
     <>
+      <h1>QuizzHub</h1>
+      <h2>Question</h2>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {buttonLabels.map((label, index) => (
+          <Button key={index} label={label} handler={checkAnswer} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
